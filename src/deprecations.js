bde4fba5018e0ab3a2a6fb0001c3d45f7b63da97
Eric Cornelissen <ericornelissen@gmail.com>
2025-01-20 23:17:11 +0100

    Add `--omit` flag to omit dev, optional, or peer dependencies
    Signed-off-by: Eric Cornelissen <ericornelissen@gmail.com>

--- src/deprecations.js
+++ src/deprecations.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2024  Eric Cornelissen
+// Copyright (C) 2024-2025  Eric Cornelissen
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
@@ -14,7 +14,13 @@
 
 import { spawn } from "node:child_process";
 
-export async function obtainDeprecations() {
+/**
+ * Obtain all deprecation warnings for the current project.
+ *
+ * @param {NpmCliOptions} options The configuration for deprecations to get.
+ * @returns {Promise<Deprecation[]>} A list of deprecation warnings.
+ */
+export async function obtainDeprecations(options) {
 	return new Promise((resolve, reject) => {
 		const process = spawn(
 			"npm",
@@ -23,6 +29,9 @@ export async function obtainDeprecations() {
 				"--no-audit",
 				"--no-fund",
 				"--no-update-notifier",
+				...(options.omitDev ? ["--omit", "dev"] : []),
+				...(options.omitOptional ? ["--omit", "optional"] : []),
+				...(options.omitPeer ? ["--omit", "peer"] : []),
 			],
 			{
 				shell: false,
@@ -51,14 +60,32 @@ export async function obtainDeprecations() {
 
 const prefix = Buffer.from("npm warn deprecated ");
 
+/**
+ * Determine if a line from the npm CLI is a deprecation warning.
+ *
+ * @param {string} line A line from the npm CLI.
+ * @returns {boolean} `true` if the line is a deprecation warning.
+ */
 function isDeprecationWarning(line) {
 	return line.slice(0, prefix.length).equals(prefix);
 }
 
+/**
+ * Extract the deprecation warning content from a line outputted by the npm CLI.
+ *
+ * @param {string} line The raw line outputted by the npm CLI.
+ * @returns {string} The line without the deprecation warning prefix.
+ */
 function extractDeprecation(line) {
 	return line.slice(prefix.length, line.length).toString();
 }
 
+/**
+ * Parse a raw deprecation warning outputted by the npm CLI.
+ *
+ * @param {string} str The deprecation warning.
+ * @returns {Deprecation} The parsed deprecation.
+ */
 function parseDeprecation(str) {
 	let pkg, reason, name, version;
 
@@ -77,6 +104,28 @@ function parseDeprecation(str) {
 	return { name, version, reason };
 }
 
+/**
+ * A filter function to remove duplicate {@link Deprecation} objects.
+ *
+ * @param {Deprecation} a An element of `array`.
+ * @param {number} index The index of element `a` in `array`.
+ * @param {Deprecation[]} array The full array to filter.
+ * @returns {boolean} `true` if `a` is the first occurrence in `array`.
+ */
 function unique(a, index, array) {
 	return index === array.findIndex(b => a.name === b.name && a.version === b.version);
 }
+
+/**
+ * @typedef NpmCliOptions
+ * @property {boolean} omitDev Set `--omit dev`.
+ * @property {boolean} omitOptional Set `--omit optional`.
+ * @property {boolean} omitPeer Set `--omit peer`.
+ */
+
+/**
+ * @typedef Deprecation
+ * @property {string} name The name of the deprecated package.
+ * @property {string} version The version of the deprecated package.
+ * @property {string} reason The deprecation message.
+ */
