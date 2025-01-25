d12337ef67074ad46747c97adab788d59f8de073
Eric Cornelissen <ericornelissen@gmail.com>
2025-01-25 22:52:42 +0100

    Improve `config.js` and `deprecations.js` module test suites
    Update existing test suites in order to improve test quality,
    convenience, and coverage.
    
    Signed-off-by: Eric Cornelissen <ericornelissen@gmail.com>

--- src/deprecations.test.js
+++ src/deprecations.test.js
@@ -43,7 +43,6 @@ test("deprecations.js", async (t) => {
 				installLog: [
 					"npm warn deprecated foobar@3.1.4: This package is no longer supported.",
 				],
-				manifest: {},
 				options: defaultOptions,
 				want: [
 					{
@@ -94,20 +93,16 @@ test("deprecations.js", async (t) => {
 			await t.test(name, async () => {
 				const { options, want } = testCase;
 
-				const fs = createFs({
-					"./package.json": JSON.stringify(testCase.manifest),
-				});
 				const cp = createCp({
 					"npm clean-install": {
-						error: null,
-						stdout: "",
 						stderr: testCase.installLog.join("\n"),
 					},
 					"npm list --all --json": {
-						error: null,
 						stdout: JSON.stringify(testCase.hierarchy),
-						stderr: "",
 					},
+				});
+				const fs = createFs({
+					"./package.json": JSON.stringify(testCase.manifest || {}),
 				});
 
 				const got = await getDeprecatedPackages({ cp, fs, options });
@@ -115,70 +110,284 @@ test("deprecations.js", async (t) => {
 			});
 		}
 
+		await t.test("options", async (t) => {
+			function setup() {
+				return {
+					cp: createCp({
+						"npm clean-install": {},
+						"npm list --all --json": { stdout: "{}", },
+					}),
+					fs: createFs({
+						"./package.json": "{}",
+					}),
+				};
+			}
+
+			await t.test("omitDev", async () => {
+				const { cp, fs } = setup();
+
+				const options = {
+					...defaultOptions,
+					omitDev: true,
+				};
+
+				await getDeprecatedPackages({ cp, fs, options })
+				assert.ok(cp.spawn.mock.calls[0].arguments[1].join(" ").includes("--omit dev"));
+				assert.ok(cp.exec.mock.calls[0].arguments[0].includes("--omit dev"));
+			});
+
+			await t.test("omitOptional", async () => {
+				const { cp, fs } = setup();
+
+				const options = {
+					...defaultOptions,
+					omitOptional: true,
+				};
+
+				await getDeprecatedPackages({ cp, fs, options })
+				assert.ok(cp.spawn.mock.calls[0].arguments[1].join(" ").includes("--omit optional"));
+				assert.ok(cp.exec.mock.calls[0].arguments[0].includes("--omit optional"));
+			});
+
+			await t.test("omitPeer", async () => {
+				const { cp, fs } = setup();
+
+				const options = {
+					...defaultOptions,
+					omitPeer: true,
+				};
+
+				await getDeprecatedPackages({ cp, fs, options })
+				assert.ok(cp.spawn.mock.calls[0].arguments[1].join(" ").includes("--omit peer"));
+				assert.ok(cp.exec.mock.calls[0].arguments[0].includes("--omit peer"));
+			});
+		});
+
 		await t.test("deprecation warnings cannot be obtained", async () => {
+			const options = defaultOptions;
+
 			const cp = createCp({
-				"npm clean-install": {
-					error: true,
-					stdout: "",
-					stderr: "",
-				},
-				"npm list --all --json": {
-					error: null,
-					stdout: "{}",
-					stderr: "",
-				},
+				"npm clean-install": { error: true },
+				"npm list --all --json": { stdout: "{}" },
 			});
 			const fs = createFs({
 				"./package.json": "{}",
 			});
-			const options = defaultOptions;
 
-			await assert.rejects(async () => {
-				await getDeprecatedPackages({ cp, fs, options })
-			});
+			await assert.rejects(
+				() => getDeprecatedPackages({ cp, fs, options }),
+			);
 		});
 
 		await t.test("dependency hierarchy cannot be obtained", async () => {
+			const options = defaultOptions;
+
 			const cp = createCp({
-				"npm clean-install": {
-					error: null,
-					stdout: "",
-					stderr: "",
-				},
+				"npm clean-install": {},
 				"npm list --all --json": {
 					error: true,
 					stdout: "{}",
-					stderr: "",
 				},
 			});
 			const fs = createFs({
 				"./package.json": "{}",
 			});
-			const options = defaultOptions;
 
-			await assert.rejects(async () => {
-				await getDeprecatedPackages({ cp, fs, options })
-			});
+			await assert.rejects(
+				() => getDeprecatedPackages({ cp, fs, options }),
+			);
 		});
 
 		await t.test("aliases cannot be obtained", async () => {
-			const cp = createCp({
-				"npm clean-install": {
-					error: null,
-					stdout: "",
-					stderr: "",
-				},
-				"npm list --all --json": {
-					error: null,
-					stdout: "{}",
-					stderr: "",
-				},
-			});
-			const fs = createFs({});
 			const options = defaultOptions;
 
-			await assert.rejects(async () => {
-				await getDeprecatedPackages({ cp, fs, options })
+			const cp = createCp({
+				"npm clean-install": {},
+				"npm list --all --json": { stdout: "{}" },
+			});
+			const fs = createFs({});
+
+			await assert.rejects(
+				() => getDeprecatedPackages({ cp, fs, options }),
+			);
+		});
+	});
+
+	await t.test("createCp", async (t) => {
+		await t.test("exec", async (t) => {
+			await t.test("command found", async (t) => {
+				await t.test("fully specified", () => {
+					const cmd = "foobar";
+					const error = new Error();
+					const stdout = "Hello";
+					const stderr = "world!";
+
+					const cp = createCp({
+						[cmd]: { error, stdout, stderr },
+					});
+
+					cp.exec(cmd, {}, (...got) => {
+						assert.equal(got[0], error);
+						assert.equal(got[1].toString(), stdout);
+						assert.equal(got[2].toString(), stderr);
+					});
+				});
+
+				await t.test("no error specified", () => {
+					const cmd = "foobar";
+					const stdout = "Hello";
+					const stderr = "world!";
+
+					const cp = createCp({
+						[cmd]: { stdout, stderr },
+					});
+
+					cp.exec(cmd, {}, (...got) => {
+						assert.equal(got[0], null);
+						assert.equal(got[1].toString(), stdout);
+						assert.equal(got[2].toString(), stderr);
+					});
+				});
+
+				await t.test("no stdout specified", () => {
+					const cmd = "foo";
+					const error = new Error();
+					const stderr = "bar";
+
+					const cp = createCp({
+						[cmd]: { error, stderr },
+					});
+
+					cp.exec(cmd, {}, (...got) => {
+						assert.equal(got[0], error);
+						assert.equal(got[1].toString(), "");
+						assert.equal(got[2].toString(), stderr);
+					});
+				});
+
+				await t.test("no stderr specified", () => {
+					const cmd = "foo";
+					const error = new Error();
+					const stdout = "bar";
+
+					const cp = createCp({
+						[cmd]: { error, stdout },
+					});
+
+					cp.exec(cmd, {}, (...got) => {
+						assert.equal(got[0], error);
+						assert.equal(got[1].toString(), stdout);
+						assert.equal(got[2].toString(), "");
+					});
+				});
+
+				await t.test("no stdout nor stderr specified", () => {
+					const cmd = "foobar";
+					const error = new Error();
+
+					const cp = createCp({
+						[cmd]: { error },
+					});
+
+					cp.exec(cmd, {}, (...got) => {
+						assert.equal(got[0], error);
+						assert.equal(got[1].toString(), "");
+						assert.equal(got[2].toString(), "");
+					});
+				});
+			});
+
+			await t.test("command not found", () => {
+				const cp = createCp({ foo: {} });
+				assert.ok(!cp.exec("bar"));
+			});
+		});
+
+		await t.test("spawn", async (t) => {
+			await t.test("command found", async (t) => {
+				await t.test("close handler, without error", (_, done) => {
+					const cmd = "foobar";
+
+					const cp = createCp({
+						[cmd]: {},
+					});
+
+					const process = cp.spawn(cmd, []);
+					process.on("close", (exitCode, error) => {
+						assert.equal(exitCode, 0);
+						assert.equal(error, null);
+						done();
+					});
+				});
+
+				await t.test("close handler, with error", (_, done) => {
+					const cmd = "foobar";
+					const error = new Error();
+
+					const cp = createCp({
+						[cmd]: { error },
+					});
+
+					const process = cp.spawn(cmd, []);
+					process.on("close", (exitCode, error) => {
+						assert.equal(exitCode, 1);
+						assert.equal(error, error);
+						done();
+					});
+				});
+
+				await t.test("stdout handler", (_, done) => {
+					const cmd = "foobar";
+					const stdout = "Hello world!";
+
+					const cp = createCp({
+						[cmd]: { stdout },
+					});
+
+					const process = cp.spawn(cmd, []);
+					process.on("close", done);
+					process.stdout.on("data", (data) => {
+						assert.equal(data.toString(), stdout);
+					});
+				});
+
+				await t.test("stderr handler", (_, done) => {
+					const cmd = "foobar";
+					const stderr = "Hello world!";
+
+					const cp = createCp({
+						[cmd]: { stderr },
+					});
+
+					const process = cp.spawn(cmd, []);
+					process.on("close", done);
+					process.stderr.on("data", (data) => {
+						assert.equal(data.toString(), stderr);
+					});
+				});
+			});
+
+			await t.test("command not found", () => {
+				const cp = createCp({ foo: {} });
+				assert.ok(!cp.spawn("bar", ["baz"]));
+			});
+		});
+	});
+
+	await t.test("createFs", async (t) => {
+		await t.test("readFile", async (t) => {
+			await t.test("file found", async () => {
+				const name = "foo";
+				const content = "bar";
+
+				const fs = createFs({ [name]: content });
+				const got = await fs.readFile(name);
+				assert.equal(got.toString(), content);
+			});
+
+			await t.test("file not found", async () => {
+				const fs = createFs({});
+				await assert.rejects(() => fs.readFile("foobar"));
 			});
 		});
 	});
@@ -195,9 +404,9 @@ function createCp(commands) {
 				if (cmd.includes(command)) {
 					const { error, stdout, stderr } = result;
 					callback(
-						error,
-						Buffer.from(stdout),
-						Buffer.from(stderr),
+						error || null,
+						Buffer.from(stdout || ""),
+						Buffer.from(stderr || ""),
 					);
 
 					break;
@@ -209,8 +418,8 @@ function createCp(commands) {
 				if (`${cmd} ${args.join(" ")}`.includes(command)) {
 					const { error, stdout, stderr } = result;
 
-					const outLines = stdout.split("\n");
-					const errLines = stderr.split("\n");
+					const outLines = (stdout || "").split("\n");
+					const errLines = (stderr || "").split("\n");
 
 					const handlers = {};
 					const process = {
@@ -221,14 +430,19 @@ function createCp(commands) {
 
 					const id = setInterval(() => {
 						if (outLines.length > 0) {
-							const line = Buffer.from(outLines.shift());
-							handlers.stdout?.(line);
+							const line = outLines.shift();
+							const data = Buffer.from(line);
+							handlers.stdout?.(data);
 						} else if (errLines.length > 0) {
-							const line = Buffer.from(errLines.shift());
-							handlers.stderr?.(line);
+							const line = errLines.shift();
+							const data = Buffer.from(line);
+							handlers.stderr?.(data);
 						} else {
-							handlers.close?.(error ? 1 : 0);
-							clearTimeout(id);
+							try {
+								handlers.close?.(error ? 1 : 0, error || null);
+							} finally {
+								clearTimeout(id);
+							}
 						}
 					}, 1);
 
