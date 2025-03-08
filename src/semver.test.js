308c0709914fd9e4fa190ebb35d78290ca430615
Eric Cornelissen <ericornelissen@gmail.com>
2025-03-08 17:06:51 +0100

    Create explicit facade for the `semver` dependency
    Refactor the code base to hide the dependency on semver behind an
    (explicit) facade. This improves upon the prior situation by making it
    more explicit (because it is centralized) how the dependency is used.
    
    Creating a facade like this has several benefits. First, it allows us to
    easily switch out the dependency (or create a homegrown implementation)
    without having to refactor outside of this one file. Second (and
    complementary), we can have an isolated test suite that ensures all the
    functionality we expect from semver works as expected, which is useful
    when upgrading the dependency on semver (or when swapping it out for an
    alternative).
    
    The situation before could be considered an implicit facade because the
    dependency was only ever used in one location anyway. By making it
    explicit we ensure that the clarity of the facade is maintained
    indefinitely going forward.
    
    Signed-off-by: Eric Cornelissen <ericornelissen@gmail.com>

---
+++ src/semver.test.js
@@ -0,0 +1,436 @@
+// Copyright (C) 2025  Eric Cornelissen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, version 3 of the License only.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import * as assert from "node:assert/strict";
+import { test } from "node:test";
+
+import {
+	satisfies,
+} from "./semver.js";
+
+test("semver.js", async (t) => {
+	await t.test("satisfies", async (t) => {
+		const testCases = {
+			...{
+				"no range: exact match": {
+					version: "3.1.4",
+					range: "3.1.4",
+					want: true,
+				},
+				"no range: before, patch": {
+					version: "3.1.4",
+					range: "3.1.5",
+					want: false,
+				},
+				"no range: before, minor": {
+					version: "3.0.5",
+					range: "3.1.4",
+					want: false,
+				},
+				"no range: before, major": {
+					version: "2.7.1",
+					range: "3.1.4",
+					want: false,
+				},
+				"no range: after, patch": {
+					version: "3.1.5",
+					range: "3.1.4",
+					want: false,
+				},
+				"no range: after, minor": {
+					version: "3.2.1",
+					range: "3.1.4",
+					want: false,
+				},
+				"no range: after, major": {
+					version: "4.0.0",
+					range: "3.1.4",
+					want: false,
+				},
+			},
+			...{
+				"= range: exact match": {
+					version: "3.1.4",
+					range: "=3.1.4",
+					want: true,
+				},
+				"= range: before, patch": {
+					version: "3.1.4",
+					range: "=3.1.5",
+					want: false,
+				},
+				"= range: before, minor": {
+					version: "3.0.5",
+					range: "=3.1.4",
+					want: false,
+				},
+				"= range: before, major": {
+					version: "2.7.1",
+					range: "=3.1.4",
+					want: false,
+				},
+				"= range: after, patch": {
+					version: "3.1.5",
+					range: "=3.1.4",
+					want: false,
+				},
+				"= range: after, minor": {
+					version: "3.2.1",
+					range: "=3.1.4",
+					want: false,
+				},
+				"= range: after, major": {
+					version: "4.0.0",
+					range: "=3.1.4",
+					want: false,
+				},
+			},
+			...{
+				"> range: earliest match": {
+					version: "3.1.4",
+					range: ">3.1.4",
+					want: false,
+				},
+				"> range: before, patch": {
+					version: "3.1.4",
+					range: ">3.1.5",
+					want: false,
+				},
+				"> range: before, minor": {
+					version: "3.0.5",
+					range: ">3.1.4",
+					want: false,
+				},
+				"> range: before, major": {
+					version: "2.7.1",
+					range: ">3.1.4",
+					want: false,
+				},
+				"> range: after, patch": {
+					version: "3.1.5",
+					range: ">3.1.4",
+					want: true,
+				},
+				"> range: after, minor": {
+					version: "3.2.1",
+					range: ">3.1.4",
+					want: true,
+				},
+				"> range: after, major": {
+					version: "4.0.0",
+					range: ">3.1.4",
+					want: true,
+				},
+			},
+			... {
+				">= range: earliest match": {
+					version: "3.1.4",
+					range: ">=3.1.4",
+					want: true,
+				},
+				">= range: before, patch": {
+					version: "3.1.4",
+					range: ">=3.1.5",
+					want: false,
+				},
+				">= range: before, minor": {
+					version: "3.0.5",
+					range: ">=3.1.4",
+					want: false,
+				},
+				">= range: before, major": {
+					version: "2.7.1",
+					range: ">=3.1.4",
+					want: false,
+				},
+				">= range: after, patch": {
+					version: "3.1.5",
+					range: ">=3.1.4",
+					want: true,
+				},
+				">= range: after, minor": {
+					version: "3.2.1",
+					range: ">=3.1.4",
+					want: true,
+				},
+				">= range: after, major": {
+					version: "4.0.0",
+					range: ">=3.1.4",
+					want: true,
+				},
+			},
+			...{
+				...{
+					"^ range: earliest match": {
+						version: "3.1.4",
+						range: "^3.1.4",
+						want: true,
+					},
+					"^ range: before, patch": {
+						version: "3.1.4",
+						range: "^3.1.5",
+						want: false,
+					},
+					"^ range: before, minor": {
+						version: "3.0.5",
+						range: "^3.1.4",
+						want: false,
+					},
+					"^ range: before, major": {
+						version: "2.7.1",
+						range: "^3.1.4",
+						want: false,
+					},
+					"^ range: after, patch": {
+						version: "3.1.5",
+						range: "^3.1.4",
+						want: true,
+					},
+					"^ range: after, minor": {
+						version: "3.2.1",
+						range: "^3.1.4",
+						want: true,
+					},
+					"^ range: after, major": {
+						version: "4.0.0",
+						range: "^3.1.4",
+						want: false,
+					},
+				},
+				...{
+					"^ range: pre-v1, earliest match": {
+						version: "0.4.2",
+						range: "^0.4.2",
+						want: true,
+					},
+					"^ range: pre-v1, before, patch": {
+						version: "0.4.1",
+						range: "^0.4.2",
+						want: false,
+					},
+					"^ range: pre-v1, before, minor": {
+						version: "0.3.6",
+						range: "^0.4.2",
+						want: false,
+					},
+					"^ range: pre-v1, after, patch": {
+						version: "0.4.3",
+						range: "^0.4.2",
+						want: true,
+					},
+					"^ range: pre-v1, after, minor": {
+						version: "0.5.1",
+						range: "^0.4.2",
+						want: false,
+					},
+					"^ range: pre-v1, after, major": {
+						version: "1.0.0",
+						range: "^0.4.2",
+						want: false,
+					},
+				},
+			},
+			...{
+				...{
+					"~ range: full version, earliest match": {
+						version: "3.1.4",
+						range: "~3.1.4",
+						want: true,
+					},
+					"~ range: full version, before, patch": {
+						version: "3.1.4",
+						range: "~3.1.5",
+						want: false,
+					},
+					"~ range: full version, before, minor": {
+						version: "3.0.5",
+						range: "~3.1.4",
+						want: false,
+					},
+					"~ range: full version, before, major": {
+						version: "2.7.1",
+						range: "~3.1.4",
+						want: false,
+					},
+					"~ range: full version, after, patch": {
+						version: "3.1.5",
+						range: "~3.1.4",
+						want: true,
+					},
+					"~ range: full version, after, minor": {
+						version: "3.2.1",
+						range: "~3.1.4",
+						want: false,
+					},
+					"~ range: full version, after, major": {
+						version: "4.0.0",
+						range: "~3.1.4",
+						want: false,
+					},
+				},
+				...{
+					"~ range: major-minor version, earliest match": {
+						version: "3.1.0",
+						range: "~3.1",
+						want: true,
+					},
+					"~ range: major-minor version, before, minor": {
+						version: "3.0.5",
+						range: "~3.1",
+						want: false,
+					},
+					"~ range: major-minor version, before, major": {
+						version: "2.7.1",
+						range: "~3.1",
+						want: false,
+					},
+					"~ range: major-minor version, after, patch": {
+						version: "3.1.5",
+						range: "~3.1",
+						want: true,
+					},
+					"~ range: major-minor version, after, minor": {
+						version: "3.2.1",
+						range: "~3.1",
+						want: false,
+					},
+					"~ range: major-minor version, after, major": {
+						version: "4.0.0",
+						range: "~3.1",
+						want: false,
+					},
+				},
+				...{
+					"~ range: major only, earliest match": {
+						version: "3.0.0",
+						range: "~3",
+						want: true,
+					},
+					"~ range: major only, before, major": {
+						version: "2.7.1",
+						range: "~3",
+						want: false,
+					},
+					"~ range: major only, after, patch": {
+						version: "3.1.5",
+						range: "~3",
+						want: true,
+					},
+					"~ range: major only, after, minor": {
+						version: "3.2.1",
+						range: "~3",
+						want: true,
+					},
+					"~ range: major only, after, major": {
+						version: "4.0.0",
+						range: "~3",
+						want: false,
+					},
+				},
+			},
+			...{
+				...{
+					"x range: major-minor version, earliest match": {
+						version: "3.1.0",
+						range: "3.1.x",
+						want: true,
+					},
+					"x range: major-minor version, before, minor": {
+						version: "3.0.5",
+						range: "3.1.x",
+						want: false,
+					},
+					"x range: major-minor version, before, major": {
+						version: "2.7.1",
+						range: "3.1.x",
+						want: false,
+					},
+					"x range: major-minor version, after, patch": {
+						version: "3.1.5",
+						range: "3.1.x",
+						want: true,
+					},
+					"x range: major-minor version, after, minor": {
+						version: "3.2.1",
+						range: "3.1.x",
+						want: false,
+					},
+					"x range: major-minor version, after, major": {
+						version: "4.0.0",
+						range: "3.1.x",
+						want: false,
+					},
+				},
+				...{
+					"x range: major version, earliest match": {
+						version: "3.0.0",
+						range: "3.x.x",
+						want: true,
+					},
+					"x range: major version, before, major": {
+						version: "2.7.1",
+						range: "3.x.x",
+						want: false,
+					},
+					"x range: major version, after, patch": {
+						version: "3.1.5",
+						range: "3.x.x",
+						want: true,
+					},
+					"x range: major version, after, minor": {
+						version: "3.2.1",
+						range: "3.x.x",
+						want: true,
+					},
+					"x range: major version, after, major": {
+						version: "4.0.0",
+						range: "3.x.x",
+						want: false,
+					},
+				},
+				...{
+					"x range: anything, earliest match": {
+						version: "0.0.0",
+						range: "x",
+						want: true,
+					},
+					"x range: anything, pre-v1, patch": {
+						version: "0.0.7",
+						range: "x",
+						want: true,
+					},
+					"x range: anything, pre-v1, minor": {
+						version: "0.4.2",
+						range: "x",
+						want: true,
+					},
+					"x range: anything, post-v1": {
+						version: "1.2.3",
+						range: "x",
+						want: true,
+					},
+				},
+			},
+		};
+
+		for (const [name, testCase] of Object.entries(testCases)) {
+			await t.test(name, () => {
+				const { version, range, want } = testCase;
+
+				const got = satisfies(version, range);
+				assert.equal(got, want);
+			});
+		}
+	});
+});
