d155c399e8993cccba22b58386dfb08cd30b1828
Eric Cornelissen <ericornelissen@gmail.com>
2025-04-19 21:51:25 +0200

    Introduce `Option` type
    Create a new type `Option` modelled after Rust's `Option`. This follows
    the `Result` type (see `src/result.js`) but instead of being a possibly
    failed outcome, it is a possibly existing outcome. That is, an option is
    either `Some` value or `None`. The goal is to use this construct
    (explicitly) instead of using `null` and/or `undefined` (in part because
    both options are possible).
    
    Signed-off-by: Eric Cornelissen <ericornelissen@gmail.com>

---
+++ src/option.js
@@ -0,0 +1,81 @@
+// Copyright (C) 2025  Eric Cornelissen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, version 3 of the License only.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * @template T
+ * @typedef Option<T>
+ * @property {function(): boolean} isNone
+ * @property {function(): boolean} isSome
+ * @property {function(): T} value
+ */
+
+/**
+ * @class
+ * @template T
+ */
+export class Some {
+	/**
+	 * @param {T} value
+	 */
+	constructor(value) {
+		this.v = value;
+	}
+
+	/**
+	 * @returns {boolean}
+	 */
+	isNone() {
+		return false;
+	}
+
+	/**
+	 * @returns {boolean}
+	 */
+	isSome() {
+		return true;
+	}
+
+	/**
+	 * @returns {T}
+	 */
+	value() {
+		return this.v;
+	}
+}
+
+/**
+ * @type {Option<never>}
+ */
+export const None = {
+	/**
+	 * @returns {boolean}
+	 */
+	isNone() {
+		return true;
+	},
+
+	/**
+	 * @returns {boolean}
+	 */
+	isSome() {
+		return false;
+	},
+
+	/**
+	 * @throws {TypeError}
+	 */
+	value() {
+		throw new TypeError("None has no value");
+	},
+}
