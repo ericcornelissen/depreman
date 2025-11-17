4d073a04fbc368034dff0bbdde6c73c1c30d9c29
Eric Cornelissen <ericornelissen@gmail.com>
2025-11-17 22:24:38 +0100

    Implement hardening techniques against monkey patching
    Add various hardening measures against monkey patching, following [1],
    for the CLI. We can't do this for the "library part" as (future) users
    would not expect this.
    
    The hardening occurs before any (untrusted) code is imported, this is
    ensured by using the `import` function (instead of syntax) to import
    `main.js`. The specific hardenings added here are:
    
    - Deleting `__proto__` to prevent some prototype pollution attacks. Such
      attacks could in theory happen when, e.g., an unsuspecting users runs
      this tool using an untrusted configuration file.
    - Freezing `Object.prototype` to further harden against prototype
      pollution (e.g. using `constructor.prototype` instead).
    - Freezing `Array.prototype` to avoid monkey patching methods that are
      used by this tool.
    - Freezing `globalThis` to avoid monkey patching global variables that
      are used by this tool. (Note that creating a facade around global
      variables might not help because the monkey patch might occur before
      the facade gets created).
    
    lastly, the file now has a license header as the source is no longer
    trivial.
    
    [1]: https://nodejs.org/en/learn/getting-started/security-best-practices
    
    Signed-off-by: Eric Cornelissen <ericornelissen@gmail.com>

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,26 @@
 #!/usr/bin/env node
 
+// Copyright (C) 2024-2025  Eric Cornelissen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, version 3 of the License only.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { argv, exit } from "node:process";
 
-import { cli } from "../src/main.js";
+delete Object.prototype.__proto__; // eslint-disable-line no-proto
+Object.freeze(Object.prototype);
+Object.freeze(Array.prototype);
+Object.freeze(globalThis);
 
+const { cli } = await import("../src/main.js");
 const exitCode = await cli(argv);
 exit(exitCode);
